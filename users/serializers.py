2e2dfcb7e45c025b484e82db441e439fd641de6e
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-08-14 23:56:07 +0300

    24.2 done extra task

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,15 @@ from rest_framework.serializers import ModelSerializer
 from users.models import User, Payments
 
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("first_name", "last_name", "email", "phone")
-
-
 class PaymentsSerializer(ModelSerializer):
     class Meta:
         model = Payments
         fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    payments = PaymentsSerializer(many=True, source="payments_set", read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email", "phone", "payments")
