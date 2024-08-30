81db362d8c4f6364d4ecf74af8a6ff10d0f985ac
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-08-30 22:26:24 +0300

    26.2 update task last_login_check

--- users/tasks.py
+++ users/tasks.py
@@ -12,9 +12,9 @@ def last_login_check():
     users = User.objects.filter(is_active=True)
     current_date = now()
     for user in users:
-        if user.last_login is None:
-            user.last_login = user.date_joined
+        if user.last_login and current_date - user.last_login > timedelta(days=30):
+            user.is_active = False
             user.save()
-        if current_date - user.last_login > timedelta(days=30):
+        elif current_date - user.date_joined > timedelta(days=30):
             user.is_active = False
             user.save()
